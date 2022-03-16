8c408b962245d774d52a207405a8cb174eeb467d
Felipe Taranto <felipetaranto@gmail.com>
2022-03-16 15:45:46 -0300

    Delete PYTHON directory

--- PYTHON/Verificador de Whatsapp/verificador_whatsapp.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-from time import sleep
-celular = input("\n\nInvestigador de Whastapp: ")
-navegador = webdriver.Chrome()
-navegador.get('https://api.whatsapp.com/send?1=pt_BR&phone=550'+celular)
-navegador.find_element_by_xpath('//*[@id="action-button"]').click()
-sleep(1)
-navegador.find_element_by_xpath('//*[@id="fallback_block"]/div/div/a').click()
